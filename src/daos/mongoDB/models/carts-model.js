fbe340e4995c85a12f011b0c75598d93b612d0f4
Maria Agustina Santucho <asantucho35@gmail.com>
2023-06-13 18:59:53 -0300

    solucione el problema de generacion del segundo id al pushear los productos al carrito. probe todos los otros metodos del cart y funcionan bien. queda solucionar el problema del quantity

--- src/daos/mongoDB/models/carts-model.js
+++ src/daos/mongoDB/models/carts-model.js
@@ -3,19 +3,17 @@ import { productsModel } from './products-model.js';
 
 const cartsCollection = 'carts';
 
-const cartsSchema = new mongoose.Schema(
-  {
-    products: [
-      {
-        prodId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: productsModel,
-        },
-        quantity: Number,
+const cartsSchema = new mongoose.Schema({
+  products: [
+    {
+      _id: false,
+      prodId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: productsModel,
       },
-    ],
-  }
-  //{ _id: false }
-);
+      quantity: Number,
+    },
+  ],
+});
 
 export const cartsModel = mongoose.model(cartsCollection, cartsSchema);
