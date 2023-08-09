d1fb442092aa74def6bd88f226228668e2e4a84e
Maria Agustina Santucho <asantucho35@gmail.com>
2023-08-09 17:16:17 -0300

    cree middleware con funcion isAdmin para que chequee si el usuario es user o admin de acuerdo al rol asignado al momento de la creacion del mismo, y lo agregue a los routes en los metodos

--- src/routers/product-router.js
+++ src/routers/product-router.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import ProductController from '../controllers/products-controller.js';
+import { isAdmin } from '../lib/middlewares/isAdmin.js';
 
 const productController = new ProductController();
 const productRouter = Router();
 
 productRouter.get('/', productController.getAll);
 productRouter.get('/:id', productController.getById);
-productRouter.post('/', productController.create);
-productRouter.put('/:id', productController.update);
-productRouter.delete('/:id', productController.delete);
+productRouter.post('/', isAdmin, productController.create);
+productRouter.put('/:id', isAdmin, productController.update);
+productRouter.delete('/:id', isAdmin, productController.delete);
 
 export default productRouter;
