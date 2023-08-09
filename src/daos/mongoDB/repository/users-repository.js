933f9352567819b7b724a0480b256bc77186ee5a
Maria Agustina Santucho <asantucho35@gmail.com>
2023-08-09 17:05:50 -0300

    cree repository para el users y modifique el router de current para que devuelva la informacion que establecimos en el DTO

--- src/daos/mongoDB/repository/users-repository.js
+++ src/daos/mongoDB/repository/users-repository.js
@@ -1,1 +1,20 @@
-import { userManager, productManager } from '.../persistence.js';
+import { userManager } from '.../persistence.js';
+import UsersResponseDTO from '../dtos/users-dto.js';
+
+export default class UsersRepository {
+  constructor() {
+    this.userManager = userManager;
+  }
+
+  async getCurrent(id) {
+    try {
+      const user = await this.userManager.getById(id);
+      console.log('user--->', user);
+      const userDTO = new UsersResponseDTO(user);
+      return userDTO;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+}
--- src/routers/sessions-router.js
+++ src/routers/sessions-router.js
@@ -1,17 +1,26 @@
 import { Router } from 'express';
 import passport from 'passport';
 import '../lib/jwt/jwt.js';
+import UsersRepository from '../daos/mongoDB/repository/users-repository.js';
 
 const currentRouter = Router();
 
 currentRouter.get(
   '/current',
   passport.authenticate('jwtCookies', { session: false }),
-  (req, res) => {
-    if (req.user) {
-      res.json({ user: req.user });
-    } else {
-      res.status(401).json({ error: 'Unauthorized del router' });
+  async (req, res) => {
+    try {
+      if (req.user) {
+        const userId = req.user._id;
+        const usersRepository = new UsersRepository();
+        const userDTO = await usersRepository.getCurrent(userId);
+        res.json({ user: userDTO });
+      } else {
+        res.status(401).json({ error: 'Unauthorized del router' });
+      }
+    } catch (error) {
+      console.error('Error fetching user:', error);
+      res.status(500).json({ error: 'Internal server error' });
     }
   }
 );
